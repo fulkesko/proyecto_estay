15da168941071e870dfa521e5d1a2d09912d2d41
Flavio Toro B <38701988+fulkesko@users.noreply.github.com>
2018-11-13 20:34:19 -0300

    conexion

--- el py/Main.py
+++ el py/Main.py
@@ -1,3 +1,4 @@
 import mysql.connector
-db = mysql.connector.connect(host='localhost',user='root',passwd='',database='blog2')
+db = mysql.connector.connect(host='localhost',user='root',passwd='',database='hospital')
 cursor = db.cursor()
+
